f1aa814ab5dcd99985f70953f967625815e94db3
Kilhapo66 <32168987+Kilhapo66@users.noreply.github.com>
2017-10-26 16:57:03 +0100

    Add files via upload

---
+++ OpereçaoesAutometicas1.java
@@ -0,0 +1,8 @@
+package pacote1;
+public class OpereçaoesAutometicas1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.printf("%d", 4+3*(15-2));
+	}
+
+}
