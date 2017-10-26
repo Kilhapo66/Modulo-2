15910aefc23248a3534615bd6bd8cb2c53775dd2
Kilhapo66 <32168987+Kilhapo66@users.noreply.github.com>
2017-10-26 17:27:53 +0100

    Add files via upload

---
+++ ProgramaDoEx28.java
@@ -0,0 +1,13 @@
+package ProgramaDoTeclado;
+import java.io.*;
+public class ProgramaDoEx28 {
+	public static void main(String[] args)throws IOException
+{String C = "";
+System.out.print("Digite um digito:");
+InputStreamReader LerS = new InputStreamReader(System.in);
+BufferedReader GuardaS = new BufferedReader (LerS);
+C = GuardaS.readLine ();
+System.out.printf("Digitou:%d ", Integer.valueOf (C));}}
+	
+
+
