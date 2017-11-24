26e5b3e5220b909fac77193423826927a0932dc5
Kilhapo66 <32168987+Kilhapo66@users.noreply.github.com>
2017-11-24 15:50:24 +0000

    Add files via upload

---
+++ Soma_Dos_10_1ºS.java
@@ -0,0 +1,14 @@
+package For;
+public class Soma_Dos_10_1ºS {
+	public static void main(String[] args) {		
+		int soma=0;
+		for (int I=1 ; I<=10 ; I++) {
+		soma +=I;
+		System.out.println(I);
+	}
+		System.out.println(soma);
+				
+
+	}
+
+}
