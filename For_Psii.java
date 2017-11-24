32b43975fdca5fab05df8221a77fe84b04a56f84
Kilhapo66 <32168987+Kilhapo66@users.noreply.github.com>
2017-11-24 15:00:21 +0000

    Add files via upload

---
+++ For_Psii.java
@@ -0,0 +1,10 @@
+package For;
+public class For_Psii {
+	public static void main(String[] args) {
+		String mensagem = "Hoje é sexta-feira!!";
+		for (int I=1; I<=5; I++)
+		System.out.println (mensagem);
+
+	}
+
+}
